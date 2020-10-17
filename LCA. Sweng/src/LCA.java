268ead3d1b141fb668511f6554e96ae1d6135f86
bradyr6 <60605729+bradyr6@users.noreply.github.com>
2020-10-17 17:21:45 +0100

    Add files via upload

---
+++ LCA. Sweng/src/LCA.java
@@ -0,0 +1,111 @@
+public class LCA <Key extends Comparable<Key>, Value>{
+	public Node root;
+	
+	public class Node{
+		public Key key;
+		public Value value;
+		public Node leftSide, rightSide;
+		public int N;
+		
+		public Node(Key key, Value value){
+			this.value = value;
+			this.key = key;
+		}
+	}
+	//Empty Tree implementation
+		public boolean isEmpty()
+		{
+			return size() == 0;
+		}
+		public int size() 
+		{
+			  return(size(root)); 
+			}
+			private int size(Node node) 
+			{ 
+			  if (node == null) return(0); 
+			  else { 
+			    return(size(node.leftSide) + 1 + size(node.rightSide)); 
+			  } 
+			} 
+			//Put method which inserts value into tree
+			public void put(Key key, Value value){
+				root = put(root, key, value);
+			}
+			private Node put(Node x, Key key, Value value){
+				if(x == null)
+				{
+					return new Node(key, value);
+				}
+				int comp = key.compareTo(x.key);
+				if(comp < 0)
+				{
+					x.leftSide = put(x.leftSide, key, value);
+				}
+				else if(comp > 0)
+				{
+					x.rightSide = put(x.rightSide, key, value);
+				}else
+				{
+					x.value = value;
+				}
+				x.N = 1 + size(x.leftSide) + size(x.rightSide);
+				return x;
+		 	}
+			//Get method which searches by key order
+			public Value get(Key key){
+				Node x = root;
+				
+				while(x != null){
+					
+					int comp = key.compareTo(x.key);
+					if(comp < 0)
+					{
+						x = x.leftSide;
+					}
+					else if(comp > 0)
+					{
+						x = x.rightSide;
+					}
+					else{
+						return x.value;
+					}
+				}
+				return null;
+			}
+			//Function to find LowestCommonAncestor
+			public Node search(Node root, Key value1, Key value2)
+			{
+				if(root != null &&(get(value1)!=null &&get(value2)!=null))
+				{
+					
+					if(root.key.compareTo(value1) == 0 || root.key.compareTo(value2) == 0)
+					{
+						return root;
+					}
+
+					Node leftBranch = search(root.leftSide, value1,value2);
+					Node rightBranch = search(root.rightSide,value1,value2);
+					//If they both have an answer this is the LCA 
+					
+					if(leftBranch != null && rightBranch != null)
+					{
+						return root;
+					}
+					//Find & return node with value or return null if doesn't exist
+					if(leftBranch != null) 
+					{
+						return leftBranch;
+
+					}
+					else if(rightBranch != null)
+					{
+						return rightBranch;
+					}
+					else 
+						return null;
+				}
+				return null;
+			}
+
+}
---
+++ LCA. Sweng/src/LCATest.java
@@ -0,0 +1,110 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+
+class LCATest {
+	public void testIsTreeEmpty()
+	{
+		LCA<Integer, Integer> test = new LCA<Integer, Integer>();
+		boolean isEmpty = test.isEmpty();
+		assertEquals(isEmpty, true);
+		test.put(3, 6);
+		test.put(6, 12);
+		test.put(9, 15);
+		test.put(12, 18);
+		isEmpty = test.isEmpty();
+		assertEquals(isEmpty, false);
+	}
+	@Test
+	public void testTreeSize()
+	{
+		LCA<Integer, Integer> test = new LCA<Integer, Integer>();
+		assertEquals(test.size(), 0);
+		test.put(1, 1);
+		assertEquals(test.size(), 1);
+		test.put(2, 4);
+		test.put(3, 6);
+		assertEquals(test.size(), 3);
+		test.put(1, 8);
+		assertEquals(test.size(), 3); //Size doesn't change as element already exists
+	}
+	
+	@Test
+	public void testGet()
+	{
+		LCA<Integer, Integer> test = new LCA<Integer, Integer>();
+		assertNull(test.get(3));
+		test.put(3, 1);
+		assertSame(test.get(3), 1);
+		test.put(4,2);
+		test.put(5, 3);
+		test.put(6, 4);
+		assertSame(test.get(5), 3);
+		assertNull(test.get(3));
+	}
+	@Test
+	public void testFullTree()
+	{
+		LCA<Integer, Integer> test = new LCA<Integer, Integer>();
+		test.put(1, 11);
+		test.put(2, 10);
+		test.put(3, 9);
+		test.put(4, 8);
+		test.put(5, 7);
+		test.put(6, 6);
+		test.put(7, 5);
+		test.put(8, 4);
+		test.put(9, 3);
+		test.put(10, 2);
+		test.put(11, 1);
+		assertSame(test.search(test.root,18,1).key,8);
+		assertSame(test.search(test.root,18,7).key,8);
+		assertSame(test.search(test.root,12,16).key,14);	
+	}
+	
+	@Test
+	public void testTwoNodes()
+	{
+		LCA<Integer, Integer> test = new LCA<Integer, Integer>();
+		test.put(7, 1);
+		test.put(8, 2);
+		assertSame(test.search(test.root, 7, 8).key, 7);
+		assertNull(test.search(test.root, 7, 1)); //Test to see if one node doesn't exist
+	}
+	@Test
+	public void testForNonExistingNodes()
+	{
+		LCA<Integer, Integer> test = new LCA<Integer, Integer>();
+		test.put(1, 6);
+		test.put(2, 5);
+		test.put(3, 4);
+		test.put(4, 3);
+		test.put(5, 2);
+		test.put(6, 1);
+		
+		assertNull(test.search(test.root, 7, 10));
+		assertNull(test.search(test.root, 8, 9));
+		assertNull(test.search(test.root, 9, 8));
+		assertNull(test.search(test.root, 10, 7));
+	}
+
+@Test 
+	public void testSameValue()
+	{
+	LCA<Integer, Integer> test = new LCA<Integer, Integer>();
+	test.put(1, 6);
+	test.put(2, 5);
+	test.put(3, 4);
+	test.put(4, 3);
+	test.put(5, 2);
+	test.put(6, 1);
+	
+	assertSame(test.search(test.root, 1,1).key, 1);
+	assertSame(test.search(test.root, 4, 4).key, 4);
+	}
+}
